4c2373ad4e7353448245efb4c0d2bfe805eb83a0
Camilo Paez <cristiancamiloparo@ufps.edu.co>
2026-06-02 11:35:00 -0500

    crear proyecto Spring Boot REST base

---
+++ src/main/java/com/paez/paezpost2u7/apiproductos/PaezPost2U7Application.java
@@ -0,0 +1,13 @@
+package com.paez.paezpost2u7.apiproductos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaezPost2U7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaezPost2U7Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/paez/paezpost2u7/apiproductos/PaezPost2U7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paez.paezpost2u7.apiproductos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaezPost2U7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
